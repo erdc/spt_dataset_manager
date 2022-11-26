56392323968e04bad4c40a02307604788ce23e0c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-26 00:59:12 +0000

    Adding tarfile member sanitization to extractall()

--- spt_dataset_manager/dataset_manager.py
+++ spt_dataset_manager/dataset_manager.py
@@ -343,7 +343,26 @@ class CKANDatasetManager(object):
             
                         if file_format.lower() == "tar.gz":
                             with tarfile.open(local_tar_file_path) as tar:
-                                tar.extractall(extract_directory)
+                                def is_within_directory(directory, target):
+                                    
+                                    abs_directory = os.path.abspath(directory)
+                                    abs_target = os.path.abspath(target)
+                                
+                                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                                    
+                                    return prefix == abs_directory
+                                
+                                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                                
+                                    for member in tar.getmembers():
+                                        member_path = os.path.join(path, member.name)
+                                        if not is_within_directory(path, member_path):
+                                            raise Exception("Attempted Path Traversal in Tar File")
+                                
+                                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                                    
+                                
+                                safe_extract(tar, extract_directory)
                         elif file_format.lower() == "zip":
                             with zipfile.ZipFile(local_tar_file_path) \
                                     as zip_file:
